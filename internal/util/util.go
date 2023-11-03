2c1e3f62917b97eac05494ad46c05c3ed8a7edeb
gustavo-franco-sky <gustavo.franco@sky.uk>
2023-11-03 15:04:46 +0000

    Remove unnecessary gateway code

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,12 +3,12 @@ package util
 import (
 	"time"
 
-	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // ConvertToTimestamp converts a time.Time to a timestamp.Timestamp
-func ConvertToTimestamp(t time.Time) *timestamp.Timestamp {
-	return &timestamp.Timestamp{
+func ConvertToTimestamp(t time.Time) *timestamppb.Timestamp {
+	return &timestamppb.Timestamp{
 		Seconds: t.Unix(),
 		Nanos:   int32(t.Nanosecond()),
 	}
