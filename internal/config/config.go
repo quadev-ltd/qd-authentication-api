432bb8dfb05b775334512456f3ca8730230d1419
gustavo-m-franco <gusfran17@gmail>
2024-01-30 16:28:01 -0300

    Remove test code

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,32 +1,15 @@
 package config
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/appconfigdata"
 	commonAWS "github.com/quadev-ltd/qd-common/pkg/aws"
 	commonConfig "github.com/quadev-ltd/qd-common/pkg/config"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
-
-type address struct {
-	Host string
-	Port string
-}
-
-type smtp struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-}
 
 type db struct {
 	URI string
@@ -67,13 +50,6 @@ func (config *Config) Load(path string) error {
 		log.Err(fmt.Errorf("Error loading configuration file: %v", err))
 	}
 
-	generalConfigData := config.Test()
-	configReader := bytes.NewReader(generalConfigData)
-	// Merge the AWS AppConfig data into your local configuration
-	if err := viper.MergeConfig(configReader); err != nil {
-		fmt.Println("Error merging AWS AppConfig data:", err)
-	}
-
 	if err := viper.Unmarshal(&config); err != nil {
 		return fmt.Errorf("Error unmarshaling configuration: %v", err)
 	}
@@ -83,42 +59,5 @@ func (config *Config) Load(path string) error {
 	} else {
 		config.Verbose = false
 	}
-
 	return nil
-}
-
-func (config *Config) Test() []byte {
-	sess, _ := session.NewSession(
-		&aws.Config{
-			Region: aws.String("us-east-1"),
-			Credentials: credentials.NewStaticCredentials(
-				"AKIARWDSJ2CMK4KP3ASZ",
-				"uhkUFmC8GhY8Y+pjRFTnYHLp/bItt44toRKiL3ul",
-				"",
-			),
-		},
-	)
-
-	svc := appconfigdata.New(sess)
-
-	startSessionOutput, _ := svc.StartConfigurationSession(
-		&appconfigdata.StartConfigurationSessionInput{
-			ApplicationIdentifier:          aws.String("222hh4s"),
-			EnvironmentIdentifier:          aws.String("i0lf5xh"),
-			ConfigurationProfileIdentifier: aws.String("mfgfaot"),
-		},
-	)
-
-	latestConfigOutput, err := svc.GetLatestConfiguration(&appconfigdata.GetLatestConfigurationInput{
-		ConfigurationToken: startSessionOutput.InitialConfigurationToken,
-	})
-
-	if err != nil {
-		fmt.Println("Error fetching configuration:", err)
-		return nil
-	}
-
-	// Process configuration data (e.g., set environment variables)
-	configData := latestConfigOutput.Configuration
-	return configData
 }
